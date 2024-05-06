08eab15bf08297aabe56ecb951628c943ed2b8e9
RahulSharma2001 <53604829+RahulSharma2001@users.noreply.github.com>
2024-05-06 21:10:55 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const bs = require("binary-search-test");
+function SortedArrbinarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
 
-const result = bs([1, 2, 3, 4], 3);
-console.log(result);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+module.exports = SortedArrbinarySearch;
